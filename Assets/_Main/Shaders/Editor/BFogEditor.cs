48ec76d1102c1ae555b20d27dcbd352a7c8765a3
F. Burcak Duruoz <bduruoz@gmail.com>
2024-09-05 11:00:48 +0300

    Update

---
+++ Assets/_Main/Shaders/Editor/BFogEditor.cs
@@ -0,0 +1,182 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using budu;
+
+public class BFogEditor : ShaderGUI
+{
+    bool checkFog, checkBase, checkDef;
+    bool aboutFold;
+    int tempVar;
+    
+    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+    {
+        Material targetMat = materialEditor.target as Material;
+        loadMaterialVariables(targetMat);
+
+        GUIStyle style = new GUIStyle();
+
+        #region BUDU Emissive Shader Title
+        Texture banner = (Texture)AssetDatabase.LoadAssetAtPath("Assets/_Main/Shaders/Editor/GUI/BUDUFogTitle.png", typeof(Texture));
+
+        GUILayout.BeginArea(new Rect(0, 0, 512, 32));
+        {
+            EditorGUILayout.BeginHorizontal();
+            {
+                GUI.backgroundColor = bdColors.Transparent(0);
+                GUILayout.Box(banner, GUILayout.MinHeight(32));
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+        GUILayout.EndArea();
+        GUILayout.Space(28);
+        GUI.backgroundColor = bdColors.White(255);
+        #endregion
+
+        #region Base Settings
+        style.normal.background = MakeBackground(1, 1, bdColors.GrayP(18, 204));
+        style.fontSize = 16;
+        style.normal.textColor = bdColors.NexusOrange();
+
+        EditorGUILayout.BeginVertical(style);
+        checkBase = EditorGUILayout.ToggleLeft("BASE SETTINGS", checkBase, style);
+        targetMat.SetInt("_CheckBase", Convert.ToInt16(checkBase));
+        EditorGUILayout.EndVertical();
+
+        style.normal.background = MakeBackground(1, 1, bdColors.Transparent(0));
+        EditorGUILayout.BeginVertical(style);
+        if(checkBase)
+        {
+            EditorGUI.indentLevel++;
+            EditorGUILayout.Space(2);
+            MaterialProperty fog3d = ShaderGUI.FindProperty("_3D_Fog", properties);
+            
+            materialEditor.ShaderProperty(fog3d, "3D Fog");
+            EditorGUI.indentLevel--;
+        }
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.Space(1);
+        #endregion
+
+        #region Fog Settings
+        style.normal.background = MakeBackground(1, 32, bdColors.GrayP(18,204));
+        style.fontSize = 16;
+        style.normal.textColor = bdColors.NexusOrange();
+
+        EditorGUILayout.BeginVertical(style);
+        checkFog = EditorGUILayout.ToggleLeft("FOG", checkFog, style);
+        targetMat.SetInt("_CheckFog", Convert.ToInt16(checkFog));
+        EditorGUILayout.EndVertical();
+
+        style.normal.background = MakeBackground(1, 1, bdColors.Transparent(0));
+        EditorGUILayout.BeginVertical(style);
+        if(checkFog)
+        {
+            EditorGUI.indentLevel++;
+            EditorGUILayout.Space(2);
+
+            MaterialProperty fc = ShaderGUI.FindProperty("_Fog_Color", properties);
+            MaterialProperty ft = ShaderGUI.FindProperty("_Transparency", properties);
+
+            materialEditor.ShaderProperty(fc, "Fog Color");
+            materialEditor.ShaderProperty(ft, "Fog Transparency");
+
+            EditorGUILayout.Space(1);
+
+            MaterialProperty fog3d = ShaderGUI.FindProperty("_3D_Fog",properties);
+            #region 3D Fog
+            if(fog3d.floatValue > 0f)
+            {
+                // 3D
+                EditorGUILayout.HelpBox("Request 3D Layered Planes! |||||||", MessageType.Warning);
+            }
+            #endregion
+            else
+            #region 2D Fog
+            {
+                // 2D
+                MaterialProperty fogInv = ShaderGUI.FindProperty("_Depth_Invert", properties);
+                MaterialProperty fogGExp = ShaderGUI.FindProperty("_Depth_Exponential", properties);
+                MaterialProperty depthDistanmce = ShaderGUI.FindProperty("_Depth_Fade_Distance", properties);
+
+                materialEditor.ShaderProperty(fogInv, "Invert Fog");
+                materialEditor.ShaderProperty(depthDistanmce, "Depth Distance");
+                materialEditor.ShaderProperty(fogGExp, "Depth Exponential");
+            }
+            #endregion
+            EditorGUI.indentLevel--;
+        }
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.Space(1);
+        #endregion
+
+        #region Shader Defaults
+        style.normal.background = MakeBackground(1, 1, bdColors.GrayP(18, 204));
+        style.fontSize = 16;
+        style.normal.textColor = bdColors.NexusOrange();
+        EditorGUILayout.BeginVertical(style);
+        checkDef = EditorGUILayout.ToggleLeft("SHADER DEFAULTS", checkDef, style);
+        targetMat.SetInt("_CheckDef", Convert.ToInt16(checkDef));
+        EditorGUILayout.EndVertical();
+        style.normal.background = MakeBackground(1, 1, bdColors.Transparent(0));
+        EditorGUILayout.BeginVertical(style);
+        if(checkDef)
+        {
+            EditorGUI.indentLevel++;
+            EditorGUILayout.Space(2);
+
+            //materialEditor.RenderQueueField();
+            materialEditor.EnableInstancingField();
+            materialEditor.DoubleSidedGIField();
+            EditorGUI.indentLevel--;
+        }
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.Space(1);
+        #endregion
+
+        #region BUDU Copyright
+        EditorGUILayout.Space(5);
+        style.normal.textColor = bdColors.NexusOrange(153);
+        style.fontSize = 10;
+        style.fontStyle = FontStyle.Italic;
+
+        aboutFold = EditorGUILayout.Foldout(aboutFold, "© 2024 BUDU GAMES - Burçak Duruöz", toggleOnLabelClick: true, style);
+        if(aboutFold)
+        {
+            Texture budulogo = (Texture)AssetDatabase.LoadAssetAtPath("Assets/_Main/Shaders/Editor/GUI/BUDUGames_Logo_m.png", typeof(Texture));
+            GUI.backgroundColor = bdColors.GrayP(18,204);
+            style.normal.textColor += bdColors.NexusOrange();
+            style.alignment = TextAnchor.UpperCenter;
+            GUIContent gc = new GUIContent("BUDU GAMES\nBUDU Shaders\n\n© 2024 F. Burçak Duruöz", budulogo);
+            EditorGUILayout.HelpBox(gc, true);
+        }
+        #endregion
+    }
+
+    void loadMaterialVariables(Material targetMat)
+    {
+        tempVar = targetMat.GetInt("_CheckBase");
+        checkBase = tempVar == 1 ? true : false;
+
+        tempVar = targetMat.GetInt("_CheckDef");
+        checkDef = tempVar == 1 ? true : false;
+
+        tempVar = targetMat.GetInt("_CheckFog");
+        checkFog = tempVar == 1 ? true : false;
+    }
+
+    private Texture2D MakeBackground(int width, int height, Color col)
+    {
+        Color[] pix = new Color[width * height];
+        for(int i = 0; i < pix.Length; i++)
+        {
+            pix[i] = col;
+        }
+        Texture2D result = new Texture2D(width, height);
+        result.SetPixels(pix);
+        result.Apply();
+        return result;
+    }
+}
+
+
